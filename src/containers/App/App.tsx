3dc2f99b9e9d27e72b3216ee8e4a7a7a9e0c5bae
lgybetter <437675103@qq.com>
2017-07-09 22:32:30 +0800

    write the home nav

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -44,8 +44,7 @@ class App extends React.Component<AppProps, AppState> {
             theme="dark"
             mode="horizontal"
             defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-          >
+            style={{ lineHeight: '64px' }}>
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
@@ -63,21 +62,20 @@ class App extends React.Component<AppProps, AppState> {
                 mode="inline"
                 defaultSelectedKeys={['1']}
                 defaultOpenKeys={['sub1']}
-                style={{ height: '100%' }}
-              >
-                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                style={{ height: '100%' }}>
+                <SubMenu key="sub1" title={<span><Icon type="user" />用户</span>}>
                   <Menu.Item key="1">option1</Menu.Item>
                   <Menu.Item key="2">option2</Menu.Item>
                   <Menu.Item key="3">option3</Menu.Item>
                   <Menu.Item key="4">option4</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                <SubMenu key="sub2" title={<span><Icon type="laptop" />文章</span>}>
                   <Menu.Item key="5">option5</Menu.Item>
                   <Menu.Item key="6">option6</Menu.Item>
                   <Menu.Item key="7">option7</Menu.Item>
                   <Menu.Item key="8">option8</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                <SubMenu key="sub3" title={<span><Icon type="notification" />标签</span>}>
                   <Menu.Item key="9">option9</Menu.Item>
                   <Menu.Item key="10">option10</Menu.Item>
                   <Menu.Item key="11">option11</Menu.Item>
@@ -87,12 +85,12 @@ class App extends React.Component<AppProps, AppState> {
             </Sider>
             <Content style={{ padding: '0 24px', minHeight: 280 }}>
               Content
-        </Content>
+            </Content>
           </Layout>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
           Ant Design ©2016 Created by Ant UED
-    </Footer>
+        </Footer>
       </Layout>
     );
   }
